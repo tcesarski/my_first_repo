b5566431c9b8b1539007608f2824bfa05b691f27
Taylor Cesarski <tccesars@ncsu.edu>
2024-05-15 19:59:39 -0400

    first script

---
+++ First Script.R
@@ -0,0 +1,22 @@
+iris
+head(iris)
+help(seq)
+seq(1,10,2)
+sum <- 2 + 5
+sum
+data()
+class(cars)
+class(exp)
+plot(cars)
+plot(exp)
+vec <- c(1,4,7)
+class(vec)
+str(cars)
+help(hist)
+#Get a vector of values from iris
+pl <- iris$Petal.Length
+hist(pl)
+str(pl)
+typeof(pl)
+class(pl)
+hist(pl, main = "Histogram of Petal Length")
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 I'm so excited to learn about being a **Data Scientist**
 
 This change was made in *R Studio*
+
+library(gitcreds)
+gitcreds::gitcreds_set()
+
